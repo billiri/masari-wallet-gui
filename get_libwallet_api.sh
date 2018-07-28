f9fad1741d1e94deb388ac46d37003ec788b27bc
billiri <41208649+billiri@users.noreply.github.com>
2018-07-28 18:49:37 +0300

    Update get_libwallet_api.sh

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -8,12 +8,12 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $ROOT_DIR/utils.sh
 
 INSTALL_DIR=$ROOT_DIR/wallet
-MONERO_DIR=$ROOT_DIR/monero
+MONERO_DIR=$ROOT_DIR/masarisupreme
 BUILD_LIBWALLET=false
 
 # init and update monero submodule
 if [ ! -d $MONERO_DIR/src ]; then
-    git submodule init monero
+    git submodule init masarisupreme
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
